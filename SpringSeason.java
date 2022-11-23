9a6550763c019add4a9f9f280bf7c31fb9032910
omkar <omkardudhewar121@gmail.com>
2022-11-23 14:50:51 +0530

    spring season program implemented

--- SpringSeason.java
+++ SpringSeason.java
@@ -6,9 +6,10 @@ public class SpringSeason {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter month no");
-        int m = sc.nextInt();
+        int m = sc.nextInt();//month
         System.out.println("enter day no");
-        int d = sc.nextInt();
+        int d = sc.nextInt();//day
+
         boolean isSpring = (m == 3 && d >= 20) || (m == 6 && d <= 20)
                 || (m == 4 && d <= 30) || (m == 5 && d <= 31);
         System.out.println(isSpring);
