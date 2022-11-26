fabb2a1988f9dfd562192cd4aba70257660a2f29
Om1kar <108645904+Om1kar@users.noreply.github.com>
2022-11-26 19:56:56 +0530

    Update SpringSeason.java

--- SpringSeason.java
+++ SpringSeason.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class SpringSeason {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter month no");
-        int m = sc.nextInt();//month
+        int m = scanner.nextInt();//month
         System.out.println("enter day no");
         int d = sc.nextInt();//day
 
