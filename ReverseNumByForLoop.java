80868924319c25f9e2256ec74017bf7832ffea13
Om1kar <108645904+Om1kar@users.noreply.github.com>
2022-11-26 19:55:46 +0530

    Update ReverseNumByForLoop.java

--- ReverseNumByForLoop.java
+++ ReverseNumByForLoop.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class ReverseNumByForLoop {
     public static void main(String[] args) {
         System.out.println("Enter any Number");
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
         int reverse = 0;
         int remainder = 0;
-        for (int i = 0; i < num; i++) {
+        for (int i = 0; i < number; i++) {
             remainder = num % 10;
-            num = num / 10;
+            number = number / 10;
             reverse = (reverse * 10) + remainder;
 
         }
